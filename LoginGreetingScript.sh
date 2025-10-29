c73bc83da56f7717048187a9291db6e99f9c8f31
Aryanpathania07 <aryanpathania4442@gmail.com>
2025-10-29 12:05:07 +0530

    Implement system information menu in script
    Added a system information menu to interactively display date, logged-in users, and disk usage.

--- LoginGreetingScript.sh
+++ LoginGreetingScript.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
 
-echo "Welcome, student!"
-
-echo -n "Date: "
-date "+%a %b %d %H:%M"
-echo -n "Users Logged In: "
-who | wc -l
-
-echo -n "Disk Usage: "
-df / | tail -1 | awk '{print $5}'
+while true
+do
+    echo "------------------------------------"
+    echo "        SYSTEM INFORMATION MENU     "
+    echo "------------------------------------"
+    echo "1. Display Current Date & Time"
+    echo "2. Display Number of Logged-In Users"
+    echo "3. Display Disk Usage"
+    echo "4. Exit"
+    echo "------------------------------------"
+    
+    echo -n "Enter your choice (1-4): "
+    read choice
+    
+    case $choice in
+        1)
+            echo -n "Date & Time: "
+            date
+            ;;
+        
+        2)
+            echo -n "Users Logged In: "
+            who | wc -l
+            ;;
+        
+        3)
+            echo -n "Disk Usage: "
+            df / | tail -1 | awk '{print $5}'
+            ;;
+        
+        4)
+            echo "Exiting... Goodbye!"
+            exit
+            ;;
+        
+        *)
+            echo "Invalid choice! Please select again."
+            ;;
+    esac
+    
+    echo "" 
+done
